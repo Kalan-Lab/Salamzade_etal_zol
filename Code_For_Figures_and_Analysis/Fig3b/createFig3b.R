0c0b34a2dcfa758cee478899b836dd1bbf7069b1
raufs <salamzader@gmail.com>
2024-04-01 16:07:46 +0000

    update 04-01-2024

--- Code_For_Figures_and_Analysis/Fig3b/createFig3b.R
+++ Code_For_Figures_and_Analysis/Fig3b/createFig3b.R
@@ -8,5 +8,5 @@ pdf.file <- args[2]
 input.data <- read.table("Fig3b_Data.txt", sep='\t', header=T)
 
 pdf("Fig3b.pdf", height=3, width=5)
-ggplot(input.data, aes(x=Category, y=Count, fill=as.factor(Clustering))) + geom_bar(stat='identity', color='black', show.legend=F) + theme_classic() + scale_fill_manual(values=c('#818281', '#494a49')) + xlab("") + ylab("") + ylim(0, 220) + geom_hline(yintercept=216, linetype=2, color='#ab1a3e') + geom_hline(yintercept=12, linetype=2, color='#f7658a')
+ggplot(input.data, aes(x=Category, y=Count, fill=as.factor(Clustering))) + geom_bar(stat='identity', color='black', show.legend=F) + theme_classic() + scale_fill_manual(values=c('#818281', '#494a49')) + xlab("") + ylab("") + ylim(0, 220) + geom_hline(yintercept=216, linetype=2, color='#ab1a3e') + geom_hline(yintercept=11, linetype=2, color='#f7658a')
 dev.off()
